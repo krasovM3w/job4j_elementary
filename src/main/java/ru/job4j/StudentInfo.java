d416041745a62e32ed126cf566ab2e2b5ee255b1
KrasovM3w <samar.2000@bk.ru>
2022-07-06 14:12:29 +0300

    4.1. Разбор первой программы. [#159033 #319230]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vyacheslav Krasov");
+        System.out.println("21.09.2000");
+    }
+}
